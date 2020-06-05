e9cdf41db4674f2b7649573d41f72bd635514b10
Brandon Artigue <bartigue87@yahoo.com>
2020-06-04 22:05:12 -0600

    redeploy fixes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const expressJwt = require("express-jwt");
 const path = require("path");
-const port = process.env.PORT || 5000;
-const secret = process.env.SECRET || "cheese pizza crazy";
+const port = process.env.PORT || 8000;
+// const secret = process.env.SECRET || "cheese pizza crazy";
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -38,6 +38,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-port(9000, () => {
+app.listen(port, () => {
   console.log("Server is running on local port 9000");
 });
