da3ccee34dfdcf8f59b73618841da0b5b837bb16
Brandon Artigue <bartigue87@yahoo.com>
2020-06-04 19:05:07 -0600

    expense full stack

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ require("dotenv").config();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const expressJwt = require("express-jwt");
+const path = require("path");
+const port = process.env.PORT || 5000;
+const secret = process.env.SECRET || "cheese pizza crazy";
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 mongoose.connect(
-  "mongodb://localhost:27017/user-authentication",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/user-authentication",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,6 +34,10 @@ app.use((err, req, res, next) => {
   return res.send({ errMsg: err.message });
 });
 
-app.listen(9000, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+port(9000, () => {
   console.log("Server is running on local port 9000");
 });
